7d4b8c531c9314cd994e11e40b3314fda6e89507
steefdw <beatniak@gmail.com>
2015-05-12 21:44:05 +0200

    Added some calls to the Blendle class demo page. Now ready for writing the documentation.

--- public/index.php
+++ public/index.php
@@ -3,14 +3,17 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Composer autoload
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use Steefdw\Blendle\Blendle;
 
 //$search = Blendle::search('blendle');
-$search = Blendle::query('blendle')->limit(10)->find();
+$search = Blendle::query('blendle')->limit(10)->offset(10)->find();
 
-var_dump($search->next());
-var_dump($search->prev());
+var_dump($search->total());
 var_dump($search->url_fetched());
+var_dump($search->snippets());
+var_dump($search->results());
 var_dump($search->articles());
+var_dump($search->next()->articles());
+var_dump($search->prev()->articles());
