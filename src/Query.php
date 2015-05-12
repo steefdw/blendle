3c06f9ca8b1f5af6ae655430bacf6e05cc80dbd6
steefdw <beatniak@gmail.com>
2015-05-12 20:24:20 +0200

    Let's trade doc blocks in for less code repetition

--- src/Query.php
+++ src/Query.php
@@ -4,60 +4,11 @@ namespace Steefdw\Blendle;
 
 class Query {
 
-    public function offset($query)
+    public function __call($method, $query)
     {
-        $this->parameters[] = 'offset=' .$query;
+        $this->parameters[] = $method. '=' .urlencode(implode(',', $query));
         
         return $this;
     }
-        
-    public function limit($query)
-    {
-        $this->parameters[] = 'limit=' .$query;
-        
-        return $this;
-    }  
-    
-    public function max_age($query)
-    {
-        $this->parameters[] = 'max_age=' .$query;
-        
-        return $this;
-    }      
-    
-    public function min_words($query)
-    {
-        $this->parameters[] = 'min_words=' .$query;
-        
-        return $this;
-    }      
-    
-    public function max_words($query)
-    {
-        $this->parameters[] = 'max_words=' .$query;
-        
-        return $this;
-    }   
-    
-    public function category($query)
-    {
-        $this->parameters[] = 'category=' .$query;
-        
-        return $this;
-    }       
-    
-    public function provider_id($query)
-    {
-        $this->parameters[] = 'provider_id=' .$query;
-        
-        return $this;
-    }       
-    
-    public function users($query)
-    {
-        $this->parameters[] = 'users=' .$query;
-        
-        return $this;
-    } 
 
 }
