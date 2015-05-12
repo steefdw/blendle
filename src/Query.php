5ff9ae8e0a934f110d71f0d69caefed7b930b0d9
steefdw <beatniak@gmail.com>
2015-05-12 20:40:24 +0200

    Added constaints to the methods we can call in the Query class

--- src/Query.php
+++ src/Query.php
@@ -4,10 +4,24 @@ namespace Steefdw\Blendle;
 
 class Query {
 
+    private $allowed_methods = array(
+        'offset',
+        'limit',
+        'max_age',
+        'min_words',
+        'max_words',
+        'category',
+        'provider_id',
+        'users',        
+    );
+
     public function __call($method, $query)
     {
-        $this->parameters[] = $method. '=' .urlencode(implode(',', $query));
-        
+        if(in_array($method, $this->allowed_methods))
+        {
+            $this->parameters[] = $method. '=' .urlencode(implode(',', $query));
+        }
+
         return $this;
     }
 
