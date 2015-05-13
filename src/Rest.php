a699818ae6b71ad45a2b962e118875784903edf5
steefdw <beatniak@gmail.com>
2015-05-13 12:35:39 +0200

    Added an error handler for Rest::file_get()

--- src/Rest.php
+++ src/Rest.php
@@ -6,15 +6,31 @@ class Rest {
     {
         $rest = new Rest();
 
-        return $rest->file_get($url); // if we don't need more functions like ->charset('latin1')
+        return $rest->file_get($url);
     }
 
     private function file_get($url)
     {
         ini_set('default_charset', 'UTF-8');
+        
+        set_error_handler(
+            create_function(
+                '$severity, $message, $file, $line', 
+                'throw new ErrorException($message, $severity, $severity, $file, $line);'
+            )
+        );
 
-        $context  = stream_context_create($this->get_options());
-        $response = file_get_contents($url, false, $context);
+        try
+        {
+            $context  = stream_context_create($this->get_options());
+            $response = file_get_contents($url, false, $context);
+        }
+        catch(Exception $e)
+        {
+            echo $e->getMessage();
+        }
+
+        restore_error_handler();
 
         return json_decode($response);
     }
