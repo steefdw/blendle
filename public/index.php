0c8059a108dc97eb1ef44a07f56b051e3efe1e24
steefdw <beatniak@gmail.com>
2015-05-13 13:47:48 +0200

    Better demo page, showing better what you can do with the library

--- public/index.php
+++ public/index.php
@@ -8,12 +8,33 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Steefdw\Blendle\Blendle;
 
 //$search = Blendle::search('blendle');
-$search = Blendle::query('blendle')->limit(10)->offset(10)->find();
+$search = Blendle::query('blendle')
+        ->limit(10)
+        ->offset(10)
+        ->min_words(10)
+        ->max_words(100)
+        ->provider_id('economist') // doesn't work
+        ->max_age(1)               // how does this work?
+        //->category('new')
+        ->find();
 
-var_dump($search->total());
-var_dump($search->url_fetched());
-var_dump($search->snippets());
-var_dump($search->results());
-var_dump($search->articles());
-var_dump($search->next()->articles());
-var_dump($search->prev()->articles());
+?>
+Fetched: <a href="<?php echo $search->url_fetched() ?>"><?php echo $search->url_fetched() ?></a>
+Total results: <strong><?php echo $search->total() ?></strong>
+
+Result snippets:
+<?php
+foreach($search->snippets() as $snippet)
+{ 
+    echo "  - ".$snippet.PHP_EOL;
+}
+?>
+
+Methods:
+  - Get the next set of articles:     <strong>$search->next()->articles()</strong>
+  - Get the previous set of articles: <strong>$search->prev()->articles())</strong>
+  - Show the whole result set:        <strong>$search->results()</strong>
+
+The result for <strong>$search->articles()</strong>
+
+<?php var_dump($search->articles()); ?>
